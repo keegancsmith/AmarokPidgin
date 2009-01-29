b1b2362a79cc0f2ac7af8b9cf4644e443c6d71fa
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2009-01-29 16:00:37 +0200

    Basic functionality on Amarok2

--- MPRISPidgin.py
+++ MPRISPidgin.py
@@ -3,20 +3,20 @@
 # Distributed under the GPLv2
 
 import dbus, dbus.glib
-import os, sys
-from subprocess import Popen, PIPE
+import os, sys, signal
+from subprocess import Popen, PIPE, STDOUT
 
 args = ('python', sys.argv[1], 'amarok2')
-amarokpidgin = Popen(args, bufsize=1, stdin=PIPE).stdin
+amarokpidgin = Popen(args, bufsize=1, stdin=PIPE)
+
 
 def statusChanged(status, *args):
     if status[0] == 0:
         msg = 'playing'
     else:
         msg = 'stopped'
-    amarokpidgin.write(msg + '\n')
-    amarokpidgin.flush()
-    print msg
+    amarokpidgin.stdin.write(msg + '\n')
+    amarokpidgin.stdin.flush()
 
 
 def init_dbus():
@@ -25,7 +25,15 @@ def init_dbus():
                             'org.freedesktop.MediaPlayer')
 
 
+def cleanup(signum, frame):
+    amarokpidgin.send_signal(signum)
+    if signum in (signal.SIGTERM, signal.SIGKILL):
+        sys.exit(0)
+
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, cleanup)
+
     init_dbus()
 
     import gobject
